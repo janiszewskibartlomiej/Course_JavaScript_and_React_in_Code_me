501552217a37b29312cd2833d302f94b3356d7dd
janiszewskibartlomiej <janiszewski.bartlomiej@gmail.com>
2019-11-24 10:03:42 +0100

    homework_2_3

---
+++ 19_11_2019/homework_2_3.js
@@ -0,0 +1,38 @@
+function speed_test(speed, typeOfRoad='city') {
+
+const CITY_SPEED_LIMIT = 50;
+const TWO_LANE_EXPRESSWAY_SPEED_LIMIT = 120;
+const HIGHWAY_SPEED_LIMIT = 140;
+
+this.speed = speed;
+this.typeOfRoad = typeOfRoad;
+
+if (typeOfRoad === 'city') {
+    switch (speed) {
+        case speed < CITY_SPEED_LIMIT:
+            console.log('Miasto: jedzisz prawidłowo');
+            break;
+        case speed > CITY_SPEED_LIMIT:
+            console.log('Miasto: przekroczyles prędkość');
+            break;
+    }
+} else if (typeOfRoad === 'expressway') {
+    switch(speed) {
+        case speed < TWO_LANE_EXPRESSWAY_SPEED_LIMIT:
+                console.log('Eska: jedzisz prawidłowo');
+                break;
+            case speed > TWO_LANE_EXPRESSWAY_SPEED_LIMIT:
+                console.log('Eska: przekroczyles prędkość');
+                break;
+    } 
+} else if (typeOfRoad === 'highway') {
+    switch(speed) {
+        case speed < HIGHWAY_SPEED_LIMIT:
+        console.log('Autostrada: jedzisz prawidłowo');
+        break;
+        case speed > HIGHWAY_SPEED_LIMIT:
+        console.log('Autostrada: przekroczyles prędkość');
+        break;
+    }         
+}
+}
