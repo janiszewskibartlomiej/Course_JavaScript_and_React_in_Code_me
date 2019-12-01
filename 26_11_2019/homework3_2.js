89189c33791c438de6877b59cc17b1e63904c0e3
janiszewskibartlomiej <janiszewski.bartlomiej@gmail.com>
2019-12-01 17:54:49 +0100

    change

--- 26_11_2019/homework3_2.js
+++ 26_11_2019/homework3_2.js
@@ -4,12 +4,12 @@ Funkcja powinna zwrócić string stworzony ze znaków znajdujących się
  nie zostanie podany argument to domyślnie funkcja 
  jako parametr str przyjmie łańcuch 'koparka'*/
 
-function marks (chars='koparka',) {
+function marks (chars='koparka') {
     new_string = '';
     new_chars = chars.split('');
     len = new_chars.length;
     for (let i = 0; i <= len; i += 2) {
-            //console.log(new_chars[i]);
+            console.log(new_chars[i]);
             new_string += new_chars[i];
         }
     return console.log(new_string);
